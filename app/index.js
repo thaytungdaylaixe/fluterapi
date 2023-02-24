b5709ffa0ff8299fe87e508b6117f0237a9212e4
thaytungdaylaixe <thaytungdaylaixe@gmail.com>
2023-02-24 08:14:41 +0700

    Commit changes

--- app/index.js
+++ app/index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
 app.listen(port, () => {
   console.log(`Server Listening on port http://localhost:${port}`);
 });
